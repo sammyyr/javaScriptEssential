cffc4d96ee1e20f8a25b897bf2eb2e577041d173
Samir Mu <sammyyr@gmail.com>
2024-12-10 15:13:32 -0800

    adding followersCount files

---
+++ followersCount/followersCount.js
@@ -0,0 +1,19 @@
+let count = 0; //initialize count to 0
+
+function increaseCount(){
+    count++; //increment the count by 1
+    displayCount(); //Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count; //display the count in countDisplay
+}
+
+function checkCountValue(){
+    if (count === 10){
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if(count === 20){
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
