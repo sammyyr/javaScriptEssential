581cc1eef95f9767f039db4e423d7031ae930dbe
Samir Mu <sammyyr@gmail.com>
2024-12-11 12:33:59 -0800

    added user experience fields

--- productSurvey/productSurvey.js
+++ productSurvey/productSurvey.js
@@ -7,6 +7,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
    
     alert('Thank you for your valuable feedback');
 
@@ -17,6 +18,8 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userProductExperience').innerHTML = userExperience;
+
     
     document.getElementById('userInfo').style.display = 'block';
 }
